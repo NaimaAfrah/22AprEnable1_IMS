b19d2dac1b0b2c29bffdac860422ad1b0e9f27ef
NaimaAfrah <103117610+NaimaAfrah@users.noreply.github.com>
2022-05-06 13:20:29 +0100

    Create Item.Java

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.Java
@@ -0,0 +1,73 @@
+package com.qa.ims.persistence.domain;
+
+import java.util.Objects;
+
+public class Item {
+
+	private long id;
+	private String name;
+	private double price;
+
+	public Item(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public Item(long id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "id: " + id + ", name: " + name + ", price: " + price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return id == other.id && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+	
+	
+
+}
